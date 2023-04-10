ddb5abe6fa962c7c074ef3746d4885a5705b298c
RoseminSajiT <112017693+RoseminSajiT@users.noreply.github.com>
2023-04-10 11:22:43 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtCore import QObject, pyqtSignal
-
-class Foo(QObject):
-
-    closed = pyqtSignal()
-
-    range_changed = pyqtSignal(int, int, name='rangeChanged')
-
-    valueChanged = pyqtSignal([int], ['QString'])
